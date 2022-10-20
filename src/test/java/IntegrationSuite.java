33a8d619568b312380f4dd638a19e0eb6c4aaf1c
Arat Rana <aratkumarrana@gmail.com>
2022-10-21 00:36:53 +0530

    Integration suite

---
+++ src/test/java/IntegrationSuite.java
@@ -0,0 +1,7 @@
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"com.pass"})
+public class IntegrationSuite {
+}
--- src/test/java/com/pass/TestPasswordNull.java
+++ src/test/java/com/pass/TestPasswordNull.java
@@ -2,6 +2,7 @@ package com.pass;
 
 import com.pass.criteria.Criteria;
 import com.pass.criteria.PasswordCriteriaResult;
+import com.pass.criteria.PasswordLengthCriteria;
 import com.pass.validators.PasswordValidator;
 import com.pass.validators.i18n.I18nMessageResolver;
 import mockit.Injectable;
@@ -30,8 +31,10 @@ public class TestPasswordNull {
 
     @Test
     public void testPassword_NotNull() {
-        passwordValidator = new PasswordValidator(null);
-        PasswordCriteriaResult result = passwordValidator.validate("pass@123");
+        criteriaList = new ArrayList<>();
+        criteriaList.add(new PasswordLengthCriteria(1));
+        passwordValidator = new PasswordValidator(criteriaList);
+        PasswordCriteriaResult result = passwordValidator.validate("p");
         Assertions.assertTrue(result.isValid());
     }
 
