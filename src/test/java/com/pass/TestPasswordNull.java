3f9c306fb9bd43e17adf2d753be4be04d1f5094a
Arat Rana <aratkumarrana@gmail.com>
2022-10-20 22:25:58 +0530

    TDD: Not null error code and message

--- src/test/java/com/pass/TestPasswordNull.java
+++ src/test/java/com/pass/TestPasswordNull.java
@@ -16,4 +16,16 @@ public class TestPasswordNull {
         PasswordCriteriaResult result = passwordValidator.validate("pass@123");
         Assertions.assertTrue(result.isValid());
     }
+
+    @Test
+    public void testPassword_Null() {
+        PasswordCriteriaResult result = passwordValidator.validate(null);
+        Assertions.assertFalse(result.isValid());
+    }
+
+    @Test
+    public void testPassword_Null_Error_Message() {
+        PasswordCriteriaResult result = passwordValidator.validate(null);
+        Assertions.assertNotNull(passwordValidator.getMessage(result));
+    }
 }
