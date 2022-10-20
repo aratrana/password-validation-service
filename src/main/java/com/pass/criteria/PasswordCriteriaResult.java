fa3cce1f68095e9be80f4a38b4cf9c8ec3d51056
Arat Rana <aratkumarrana@gmail.com>
2022-10-20 23:17:10 +0530

    Null error message

--- src/main/java/com/pass/criteria/PasswordCriteriaResult.java
+++ src/main/java/com/pass/criteria/PasswordCriteriaResult.java
@@ -1,20 +1,26 @@
 package com.pass.criteria;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
+@Getter
+@Setter
 public class PasswordCriteriaResult {
     protected boolean valid;
+    protected List<PasswordCriteriaResultDetail> criteriaResultDetail= new ArrayList<>();
 
-    public boolean isValid() {
-        return valid;
-    }
-
-    public void setValid(boolean valid) {
-        this.valid = valid;
+    public PasswordCriteriaResult(final boolean valid, final PasswordCriteriaResultDetail detail) {
+        setValid(valid);
+        criteriaResultDetail.add(detail);
     }
 
     public PasswordCriteriaResult(final boolean valid) {
         setValid(valid);
     }
+
 }
